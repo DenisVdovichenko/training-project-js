b0c7685d620da6c5a9aaa9b6a2442b2bf86f96cb
DenisVdovichenko <128327375+DenisVdovichenko@users.noreply.github.com>
2023-06-22 19:40:31 +0300

    sdfsdf

---
+++ js/project.js
@@ -0,0 +1,29 @@
+// // Data types
+
+// // String
+// const greetings = 'Hello world!';
+// console.log(typeof greetings);
+
+// // String
+// const num = 100;
+// console.log(typeof num);
+
+// // Boolean
+// const BoolTrue = true;
+// const BoolFalse = false;
+// console.log(typeof BoolTrue);
+// console.log(typeof BoolFalse);
+
+// const someNullValue = null;
+// console.log(someNullValue);
+
+// let someUndefinedValue;
+// console.log(someUndefinedValue);
+
+// let userImg = null;
+
+//object
+const obj = {};
+const obj2 = new Object();
+console.log(typeof obj);
+console.log(typeof obj2);
