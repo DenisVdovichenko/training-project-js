66ad388e129c308af174632e74f07d5f77f5f09e
DenisVdovichenko <128327375+DenisVdovichenko@users.noreply.github.com>
2023-07-01 21:24:33 +0300

    віаіва

--- js/project.js
+++ js/project.js
@@ -59,3 +59,68 @@
 // console.log(add(15, 27, 10));
 // console.log(add(10, 20, 30));
 // console.log(add(5, 10, 15));
+
+// function makeMessage(name, price) {
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// }
+
+// Функция проверка возраста
+
+// function isAdult(age) {
+//   const passed = age >= 18;
+//   return passed;
+// }
+
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+//   return message;
+// }
+
+// Функция проверки указаного и сохраненного паролей!
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   const isMatch = password === SAVED_PASSWORD;
+//   return isMatch;
+// }
+
+// Сравнение количества в заказе и реального количества товаров на складе
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   if (available <= ordered) {
+//     message = 'Not enough goods in stock!';
+//   } else {
+//     message = 'Order is processed, our manager will contact you.';
+//   }
+//   return message;
+// }
+
+// Комбіновані оператори
+
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// Перевірка кошти на рахунку - мінус баланс на карті!
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   const totalPrice = pricePerDroid * orderedQuantity;
+//   if (totalPrice > customerCredits) {
+//     message = 'Insufficient funds!';
+//   } else {
+//     message = `You ordered ${orderedQuantity} droids, you have ${
+//       customerCredits - totalPrice
+//     } credits left`;
+//   }
+//   return message;
+// }
