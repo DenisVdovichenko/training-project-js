754d5cda8053beed921a908360b64183d7d95010
DenisVdovichenko <128327375+DenisVdovichenko@users.noreply.github.com>
2023-07-02 02:57:46 +0300

    віаіва

--- js/project.js
+++ js/project.js
@@ -162,3 +162,41 @@
 //   const isInRange = number >= start && number <= end;
 //   return isInRange;
 // }
+
+// Проверка подписки
+
+// function checkIfCanAccessContent(subType) {
+//   const canAccessContent = subType === 'pro' || 'vip' === subType;
+
+//   return canAccessContent;
+// }
+
+// Перевірка перетворення в протилежне
+
+// function isNumberNotInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   const isNotInRange = !isInRange;
+
+//   return isNotInRange;
+// }
+
+// Обчислення знижок у %
+
+// function getDiscount(totalSpent) {
+//   const BASE_DISCOUNT = 0;
+//   const BRONZE_DISCOUNT = 0.02;
+//   const SILVER_DISCOUNT = 0.05;
+//   const GOLD_DISCOUNT = 0.1;
+//   let discount;
+
+//   if (totalSpent >= 50000) {
+//     discount = GOLD_DISCOUNT;
+//   } else if (totalSpent >= 20000 && totalSpent <= 50000) {
+//     discount = SILVER_DISCOUNT;
+//   } else if (totalSpent >= 5000 && totalSpent <= 20000) {
+//     discount = BRONZE_DISCOUNT;
+//   } else {
+//     discount = BASE_DISCOUNT;
+//   }
+//   return discount;
+// }
